942c94b83867ec5e0632e8af21a8adf4ea7b1d1f
Ciaran Palmer <ciaranp@telnyx.com>
2024-03-14 18:31:28 +0000

    task(NOC-1901): improved splitting functionality

--- server/libs/services/documents/splitter/json.ts
+++ server/libs/services/documents/splitter/json.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Splitter } from './splitter.js';
 import { encode } from 'gpt-3-encoder';
-import { TelnyxBucketParagraph } from '../vectorstore/telnyx.js';
+import { TelnyxBucketChunk } from '../vectorstore/telnyx.js';
 
 import csvToJsonV2 from 'csvtojson/v2/index.js';
 
@@ -20,19 +20,55 @@ export class JSONSplitter extends Splitter {
   }
 
   /**
-   * Splits a JSON object into paragraphs of key/value pairs where the value is a string
-   * @returns A list of paragraphs derrived from the JSON object
+   * If the JSON object is an array, we split by each object
+   * If the JSON object is a single object, we split on key/value pairs
+   * @param convertFromCsv Set to true to use a CSV as input and convert it to a JSON
+   * @returns A list of splits for the JSON object provided
    */
 
-  async split(convertFromCsv = false): Promise<TelnyxBucketParagraph[]> {
-    const data = convertFromCsv
-      ? this.extractStringValues(await this.csvToJson())
-      : this.extractStringValues(this.file);
+  async split(convertFromCsv = false) {
+    const fileContent = convertFromCsv ? await this.csvToJson() : this.file;
+    const isArray = Array.isArray(fileContent);
+
+    if (isArray) return this.splitByJsonArray(fileContent);
+    else return this.splitByStringValues(fileContent);
+  }
+
+  /**
+   * Splits a JSON array into individual objects
+   * @returns A list of JSON chunks representing each object in the array
+   */
+
+  async splitByJsonArray(objects: any): Promise<TelnyxBucketChunk[]> {
+    const result = [];
+
+    for (const object of objects) {
+      const content = JSON.stringify(object, null, 2);
+
+      const chunk: TelnyxBucketChunk = {
+        heading: null,
+        content,
+        tokens: encode(content).length,
+      };
+
+      result.push(chunk);
+    }
+
+    return result;
+  }
+
+  /**
+   * Splits a JSON object into chynks of key/value pairs
+   * @returns A list of JSON key/value chunks derrived from the JSON object
+   */
+
+  async splitByStringValues(fileContent: any): Promise<TelnyxBucketChunk[]> {
+    const data = this.extractStringValues(fileContent);
 
     const result = [];
 
     for (const item of data) {
-      const chunk: TelnyxBucketParagraph = {
+      const chunk: TelnyxBucketChunk = {
         heading: item.key,
         content: item.value,
         tokens: encode(`${item.key}\n${item.value}`).length,
@@ -44,7 +80,12 @@ export class JSONSplitter extends Splitter {
     return result;
   }
 
-  public async csvToJson(): Promise<any> {
+  /**
+   * Converts a CSV input into a JSON object
+   * @returns The JSON representation of the CSV input
+   */
+
+  private async csvToJson(): Promise<any> {
     const csvAsJson = await new Promise((resolve) => {
       csvToJsonV2({ output: 'json' })
         .fromString(this.file)
@@ -55,6 +96,12 @@ export class JSONSplitter extends Splitter {
 
     return csvAsJson;
   }
+
+  /**
+   * Extacts key/value pairs from a JSON object if the value is a string
+   * @param obj The object to extract pairs from
+   * @returns A list of key/value pairs for each string value in the object
+   */
 
   private extractStringValues(obj: any): KeyValue[] {
     const result: KeyValue[] = [];
--- server/libs/services/documents/splitter/unstructured.ts
+++ server/libs/services/documents/splitter/unstructured.ts
@@ -1,6 +1,6 @@
 import { Splitter } from './splitter.js';
 import { encode } from 'gpt-3-encoder';
-import { TelnyxBucketParagraph } from '../vectorstore/telnyx.js';
+import { TelnyxBucketChunk } from '../vectorstore/telnyx.js';
 
 export class UnstructuredTextSplitter extends Splitter {
   file: string;
@@ -18,14 +18,14 @@ export class UnstructuredTextSplitter extends Splitter {
    * @returns A list of paragraphs of size chunk_size
    */
 
-  split(chunkSize = 1000): TelnyxBucketParagraph[] {
-    const result: TelnyxBucketParagraph[] = [];
+  split(chunkSize = 1000): TelnyxBucketChunk[] {
+    const result: TelnyxBucketChunk[] = [];
 
     for (let i = 0; i < this.file.length; i += chunkSize) {
       const chunkContent = this.file.substring(i, i + chunkSize);
       const encodedLength = encode(chunkContent).length;
 
-      const chunk: TelnyxBucketParagraph = {
+      const chunk: TelnyxBucketChunk = {
         heading: null,
         content: chunkContent,
         tokens: encodedLength,
